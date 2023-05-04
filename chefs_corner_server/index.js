6f2268be378932c52b06a37f7f9afa0d217b3c13
Shahriar1212 <shahriarmoin.aust@gmail.com>
2023-05-04 19:40:31 +0600

    recipie data api added

--- chefs_corner_server/index.js
+++ chefs_corner_server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors');
 const chefs = require('./data/chefs.json')
+const recipes = require('./data/recipes.json')
 
 
 app.use(cors());
@@ -17,10 +18,20 @@ app.get('/chefs', (req, res) => {
 
 app.get('/chef/:id', (req, res) => {
     const id = req.params.id;
-    // console.log(parseid);
-    const selectedChef = chefs.find(n => n.chef_id === parseInt(id));
-    console.log(selectedChef);
-    res.send(selectedChef)
+    // console.log(id);
+    const selectedChef = chefs.find(n => n.chef_id == parseInt(id));
+    const selectedRecipes = recipes.filter(n => n.chef_id == id);
+    console.log(selectedRecipes);
+    const data = [];
+    data.push(selectedChef);
+    data.push(selectedRecipes);
+    console.log(data);
+    res.send(data)
+})
+
+app.get('/recipes', (req, res) => {
+
+    res.send(chefs)
 })
 
 app.listen(port, () => {
